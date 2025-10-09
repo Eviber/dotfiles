43e57ed5d93368075b2c4b4801f9b3e43980b66c
Youva GAUDÉ <ygaude@student.42.fr>
2025-10-09 17:49:48 +0200

    nvim - Refine toggles for virtual lines and virtual text diagnostics

--- .config/nvim/lua/eviber/plugins/lsp.lua
+++ .config/nvim/lua/eviber/plugins/lsp.lua
@@ -48,22 +48,49 @@ return {
 		config = function()
 			vim.api.nvim_create_autocmd('LspAttach', {
 				callback = function(event)
-					local bufnr = event.buf
-					local function toggle_virtual_lines()
-						if vim.diagnostic.config().virtual_lines ~= true then
-							vim.diagnostic.config({ virtual_lines = true })
+					-- Open diagnostics in a floating window when jumping to them
+					vim.diagnostic.config({ jump = { float = true } })
+
+					-- Toggle virtual lines diagnostics between off, and all lines
+					local function toggle_virtual_lines_all()
+						local current_config = vim.diagnostic.config().virtual_lines
+						if current_config == true then
+							vim.diagnostic.config({ virtual_lines = false })
 						else
-							vim.diagnostic.config({ virtual_lines = { current_line = true }})
+							vim.diagnostic.config({ virtual_text = false, virtual_lines = true })
 						end
 					end
 
+					-- Toggle virtual lines diagnostics between off, and current line only
+					local function toggle_virtual_line()
+						local current_config = vim.diagnostic.config().virtual_lines
+						if type(current_config) == 'table' and current_config.current_line == true then
+							vim.diagnostic.config({ virtual_lines = false })
+						else
+							vim.diagnostic.config({ virtual_text = false, virtual_lines = { current_line = true } })
+						end
+					end
+
+					-- Toggle virtual text diagnostics
+					local function toggle_virtual_text()
+						local current_config = vim.diagnostic.config().virtual_text
+						if current_config == true then
+							vim.diagnostic.config({ virtual_text = false })
+						else
+							vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
+						end
+					end
+
+					local bufnr = event.buf
 					vim.keymap.set("n", "grn",        vim.lsp.buf.rename,         { buffer = bufnr, desc = "Rename symbol" })
 					vim.keymap.set("n", "grr",        vim.lsp.buf.references,     { buffer = bufnr, desc = "Symbol references" })
 					vim.keymap.set("n", "gra",        vim.lsp.buf.code_action,    { buffer = bufnr, desc = "Open code actions" })
 					vim.keymap.set("n", "gd",         vim.lsp.buf.definition,     { buffer = bufnr, desc = "Go to definition" })
 					vim.keymap.set("n", "<leader>K",  vim.cmd.Man,                { buffer = bufnr, desc = "Open Man page" })
 					vim.keymap.set("i", "<C-h>",      vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature information" })
-					vim.keymap.set("n", "<leader>vl", toggle_virtual_lines,       { buffer = bufnr, desc = "toggle Virtual Lines diagnostics" })
+					vim.keymap.set("n", "<leader>vL", toggle_virtual_lines_all,   { buffer = bufnr, desc = "toggle Virtual Lines diagnostics" })
+					vim.keymap.set("n", "<leader>vl", toggle_virtual_line,        { buffer = bufnr, desc = "toggle Virtual Line diagnostics" })
+					vim.keymap.set("n", "<leader>vt", toggle_virtual_text,        { buffer = bufnr, desc = "toggle Virtual Text diagnostics" })
 
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
 					if client == nil then return end
