4c8d7e9edbae46415d901b064bd0a050abed2e08
ygaude <ygaude@student.42.fr>
2025-12-15 17:57:19 +0100

    nvim - remove Mason

--- .config/nvim/lua/eviber/plugins/lsp.lua
+++ .config/nvim/lua/eviber/plugins/lsp.lua
@@ -1,50 +1,9 @@
 return {
-	-- Autocompletion
-	-- {
-	-- 	'hrsh7th/nvim-cmp',
-	-- 	event = 'InsertEnter',
-	-- 	dependencies = {
-	-- 		{'L3MON4D3/LuaSnip'},
-	-- 	},
-	-- 	opts = function(_, opts)
-	-- 		opts.sources = opts.sources or {}
-	-- 		table.insert(opts.sources, {
-	-- 			name = "lazydev",
-	-- 			group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-	-- 		})
-        -- end,
-	-- 	config = function()
-	-- 		local cmp = require('cmp')
-
-	-- 		cmp.setup({
-	-- 			sources = {
-	-- 				{name = 'nvim_lsp'},
-	-- 			},
-	-- 			mapping = cmp.mapping.preset.insert({
-	-- 				['<C-Space>'] = cmp.mapping.complete(),
-	-- 				['<C-u>'] = cmp.mapping.scroll_docs(-4),
-	-- 				['<C-d>'] = cmp.mapping.scroll_docs(4),
-	-- 			}),
-	-- 			snippet = {
-	-- 				expand = function(args)
-	-- 					vim.snippet.expand(args.body)
-	-- 				end,
-	-- 			},
-	-- 		})
-	-- 	end
-	-- },
-
-	-- LSP
 	{
 		'neovim/nvim-lspconfig',
 		version = '*',
 		cmd = 'LspInfo',
 		event = {'BufReadPre', 'BufNewFile'},
-		dependencies = {
-			-- {'hrsh7th/cmp-nvim-lsp'},
-			{'mason-org/mason.nvim'},
-			{'mason-org/mason-lspconfig.nvim'},
-		},
 		config = function()
 			vim.api.nvim_create_autocmd('LspAttach', {
 				callback = function(event)
@@ -99,16 +58,6 @@ return {
 					end
 
 				end,
-			})
-
-			require('mason').setup()
-			require('mason-lspconfig').setup({
-				ensure_installed = {},
-				automatic_enable = {
-					exclude = {
-						"rust_analyzer",
-					}
-				},
 			})
 		end
 	},
