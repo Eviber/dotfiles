4f96b2e706b38280127ba17a1c11c33a90c78997
Youva GAUDÉ <youvagaude@gmail.com>
2026-04-11 16:19:47 +0200

    nvim - Enable crates.nvim in-process LSP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -327,7 +327,18 @@ AddPack('vyfor/cord.nvim')
 -- Crates {{{
 
 AddPack('saecki/crates.nvim', 'stable')
-require('crates').setup{}
+require('crates').setup{
+	lsp = {
+		enabled = true,
+		-- on_attach = function(client, bufnr)
+		-- 	-- the same on_attach function as for your other language servers
+		-- 	-- can be ommited if you're using the `LspAttach` autocmd
+		-- end,
+		actions = true,
+		completion = true,
+		hover = true,
+	},
+}
 
 -- }}}
 
