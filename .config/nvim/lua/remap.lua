7251820ca058cd8cf1cb2197d688ee8e134fe8e1
Youva GAUDÉ <ygaude@student.42.fr>
2026-04-13 17:31:12 +0200

    nvim - add map helper to remap.lua

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,43 +1,47 @@
+local function map(mode, lhs, rhs, desc)
+	vim.keymap.set(mode, lhs, rhs, { desc = desc })
+end
+
 -- open file explorer
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = "Open NetRW" })
+map('n', '<leader>pv', vim.cmd.Ex, "Open NetRW")
 
 -- move selected up and down with j and k
--- vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = "" })
--- vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv", { desc = "" })
+-- map('v', 'J', ":m '>+1<CR>gv=gv", { desc = "" })
+-- map('v', 'K', ":m '>-2<CR>gv=gv", { desc = "" })
 
 -- keep cursor in place when pressing J
--- vim.keymap.set('n', 'J', "mzJ`z")
+-- map('n', 'J', "mzJ`z")
 
 -- center cursor when moving half a page up/down
--- vim.keymap.set('n', '<C-d>', "<C-d>zz", { desc = "" })
--- vim.keymap.set('n', '<C-u>', "<C-u>zz", { desc = "" })
+-- map('n', '<C-d>', "<C-d>zz", { desc = "" })
+-- map('n', '<C-u>', "<C-u>zz", { desc = "" })
 
 -- center cursor when moving to searched keyword and unfold as needed
--- vim.keymap.set('n', 'n', "nzzzv")
--- vim.keymap.set('n', 'N', "Nzzzv")
+-- map('n', 'n', "nzzzv")
+-- map('n', 'N', "Nzzzv")
 
 -- paste over selected without losing current buffer
-vim.keymap.set('x', '<leader>p', '"_dP')
+map('x', '<leader>p', '"_dP')
 
 -- copy into clipboard
-vim.keymap.set('n', '<leader>y', '"+y', { desc = "Copy to clipboard" })
-vim.keymap.set('v', '<leader>y', '"+y', { desc = "Copy to clipboard" })
+map('n', '<leader>y', '"+y', "Copy to clipboard")
+map('v', '<leader>y', '"+y', "Copy to clipboard")
 
 -- delete without copying
-vim.keymap.set('n', '<leader>d', '"_d', { desc = "Delete without yanking" })
-vim.keymap.set('v', '<leader>d', '"_d', { desc = "Delete without yanking" })
+map('n', '<leader>d', '"_d', "Delete without yanking")
+map('v', '<leader>d', '"_d', "Delete without yanking")
 
 -- center cursor when navigating with Quickfix
--- vim.keymap.set('n', '<C-j>', "<cmd>cprev<CR>zz")
--- vim.keymap.set('n', '<C-k>', "<cmd>cnext<CR>zz")
--- vim.keymap.set('n', '<leader>k', "<cmd>lprev<CR>zz")
--- vim.keymap.set('n', '<leader>j', "<cmd>lprev<CR>zz")
+-- map('n', '<C-j>', "<cmd>cprev<CR>zz")
+-- map('n', '<C-k>', "<cmd>cnext<CR>zz")
+-- map('n', '<leader>k', "<cmd>lprev<CR>zz")
+-- map('n', '<leader>j', "<cmd>lprev<CR>zz")
 
-vim.keymap.set('n', '<leader>n', vim.cmd.bn, { desc = "Next buffer" })
-vim.keymap.set('n', '<leader>N', vim.cmd.bN, { desc = "Previous buffer" })
+map('n', '<leader>n', vim.cmd.bn, "Next buffer")
+map('n', '<leader>N', vim.cmd.bN, "Previous buffer")
 
 -- start a search and replace on the current word
-vim.keymap.set('n', '<leader>sr', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gc<Left><Left><Left>", { desc = "Search and replace current word" })
+map('n', '<leader>sr', ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gc<Left><Left><Left>", "Search and replace current word")
 
 -- toggle dark and light background
 local function toggle_dark()
@@ -47,9 +51,9 @@ local function toggle_dark()
 		vim.o.background = 'dark'
 	end
 end
-vim.keymap.set('n', '<leader>b', toggle_dark, { desc = "Toggle dark/light theme" })
+map('n', '<leader>b', toggle_dark, "Toggle dark/light theme")
 
-vim.keymap.set('n', '<leader>lw', function() vim.cmd("set wrap!") end, { desc = "Toggle line wrap" })
+map('n', '<leader>lw', function() vim.cmd("set wrap!") end, "Toggle line wrap")
 
 if vim.g.neovide then
 	vim.g.neovide_scale_factor = 1.0
@@ -57,10 +61,10 @@ if vim.g.neovide then
 		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
 		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', true)
 	end
-	vim.keymap.set("n", "<C-=>", function()
+	map("n", "<C-=>", function()
 		change_scale_factor(1.05)
 	end)
-	vim.keymap.set("n", "<C-->", function()
+	map("n", "<C-->", function()
 		change_scale_factor(1/1.05)
 	end)
 end
