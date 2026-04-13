8b2bc498279c3d5db3fb33be6425e5f7974e9ba3
Youva GAUDÉ <ygaude@student.42.fr>
2026-04-13 17:17:47 +0200

    nvim - revisit set options
    Surprisingly not much was unneeded or incorrect

--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -1,10 +1,12 @@
-vim.opt.compatible = false
+vim.g.mapleader = ' '
+
+-- vim.opt.compatible = false -- Defaults to true
 
 vim.opt.number = true
 vim.opt.relativenumber = false
 
 vim.opt.list = true
-vim.opt.listchars = { tab = '| ', trail = '·', precedes = '«', extends = '»' }
+vim.opt.listchars = { tab = '│ ', trail = '•', precedes = '⟨', extends = '⟩' }
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -23,18 +25,18 @@ vim.opt.undofile = true
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
+-- vim.opt.termguicolors = true -- Defaults to true if possible
 
 vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 16
 
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "121"
 
-vim.opt.completeopt = { "menuone", "noselect", "popup", "fuzzy" }
+vim.opt.completeopt = { "menuone", "noselect", "popup", "fuzzy" } -- Honestly not sure if needed
 
-vim.g.mapleader = ' '
-vim.g.rustfmt_autosave = 1
+-- vim.g.rustfmt_autosave = 1 -- Does it work? Is it needed?
 
 vim.opt.mouse = ''
 
@@ -44,24 +46,21 @@ vim.api.nvim_command('filetype plugin on')
 vim.api.nvim_command('filetype indent on')
 
 vim.diagnostic.config({
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] =  " ",
-            [vim.diagnostic.severity.HINT] =  "󰌶 ",
-        }
-    }
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] =  " ",
+			[vim.diagnostic.severity.HINT] =  "󰌶 ",
+		}
+	}
 })
 
 -- Highlight on yank
-local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-
-autocmd("TextYankPost", {
-  callback = function()
-    vim.hl.on_yank({higroup='Visual'})
-  end,
-  desc = "Briefly highlight yanked text",
-  group = augroup("yank", { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.hl.on_yank({ higroup = 'Visual' })
+	end,
+	desc = "Briefly highlight yanked text",
+	group = vim.api.nvim_create_augroup("yank", { clear = true }),
 })
